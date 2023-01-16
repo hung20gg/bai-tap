6bfc3e5e051c91044b581b3a2c1f9c027464d8d0
lienpham04 <122430491+lienpham04@users.noreply.github.com>
2023-01-17 01:16:04 +0700

    Add files via upload

---
+++ buoi 1/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  lienc
+//
+//  Created by Lien Pham on 16/01/2023.
+//
+
+# include<stdio.h>
+
+
+int main()
+{
+    int a,b;
+    printf ("Cho a = "); scanf("%d", &a);
+    printf ("Cho b = "); scanf("%d", &b);
+    int sum=a+b;
+    printf ("\n%d + %d = %d", a,b,sum);
+    return 0;
+
+}
