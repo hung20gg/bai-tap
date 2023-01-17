bb59af4a11294f012b8f15e4e7a6a8c9285de311
hung20gg <quanghung20gg@gmail.ocm>
2023-01-17 11:27:37 +0700

    ok

--- buoi 1/bai_3_hung.c
+++ buoi 1/bai_3_hung.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 int main(){
-    float a,b;
+    float a,b,a1,b1;
     printf("Length: ");scanf("%f",&a);
     printf("Width: ");scanf("%f",&b);
-    float dien_tich=a*b;
+    printf("House length: ");scanf("%f",&a1);
+    printf("House width: ");scanf("%f",&b1);
+    float dien_tich=a*b-a1*b1;
     float time=dien_tich/0.5;
+    
     printf("Time: %f second",time);
 }
