b965a873519599b085479d513644ae92c8651615
hung20gg <quanghung20gg@gmail.ocm>
2023-01-26 16:10:10 +0700

    bai2

---
+++ buoi 2/bai2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int c_at_depth(float depth){ //f(x)
+    float c= 10*depth+20;
+    printf("Do sau: %f\n",c);
+    return c;
+}
+int to_fahrenheit(float c){
+    float f=1.8*(float)c+32;
+    printf("Do F %f\n",f);
+    return f;
+}
+int main(){
+    float d;
+    //tinh do c
+    scanf("%f",&d);
+    float e=c_at_depth(d); //f(d)
+    to_fahrenheit(e);
+    
+    return 0;
+}
